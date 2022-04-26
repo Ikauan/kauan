2fd4d3972384d92afdd2caf5d09e4d9f8177aedb
KAUAN MARTINS CARDOSO <kauan.mcardoso@senacsp.edu.br>
2022-04-26 17:21:33 -0300

    Adicionar arquivos de projeto.

---
+++ kauan/View/main.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kauan.View
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Board.Text = string.Empty;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Abrir Arquivo";
+            openFileDialog1.FileName = "";
+            openFileDialog1.InitialDirectory = "C:\\";
+            openFileDialog1.Filter = "Arquivos txt|*.txt";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.StreamReader leitor = new System.IO.StreamReader(openFileDialog1.FileName);
+                Board.Text = leitor.ReadToEnd();
+                leitor.Close();
+            }
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Title = "Salvar Como";
+            saveFileDialog1.FileName = "";
+            saveFileDialog1.InitialDirectory = "C:\\";
+            saveFileDialog1.Filter = "Arquivos txt|*.txt";
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.StreamWriter gravar = new System.IO.StreamWriter(saveFileDialog1.FileName);
+                gravar.Write(Board.Text);
+                gravar.Close();
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (Board.Text == "")
+            {
+                MessageBox.Show("Marque algo.");
+            }
+            else
+            {
+                Clipboard.SetText(Board.SelectedText);
+                Board.SelectedText = "";
+            }
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (Board.SelectedText == "")
+            {
+                MessageBox.Show("Marque algo.");
+            }
+            else
+            {
+                Clipboard.SetText(Board.SelectedText);
+            }
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Board.SelectedText = Clipboard.GetText();
+        }
+
+        private void selectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Board.SelectAll();
+        }
+
+        private void customizeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (fontDialog1.ShowDialog().Equals(DialogResult.OK))
+            {
+                Board.SelectionFont = fontDialog1.Font;
+            }
+        }
+
+        private void optionsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog().Equals(DialogResult.OK))
+            {
+                Board.SelectionColor = colorDialog1.Color;
+            }
+        }
+
+        private void newToolStripButton_Click(object sender, EventArgs e)
+        {
+            newToolStripMenuItem.PerformClick();
+        }
+
+        private void openToolStripButton_Click(object sender, EventArgs e)
+        {
+            openToolStripMenuItem.PerformClick();
+        }
+
+        private void saveToolStripButton_Click(object sender, EventArgs e)
+        {
+            saveAsToolStripMenuItem.PerformClick();
+        }
+
+        private void cutToolStripButton_Click(object sender, EventArgs e)
+        {
+            cutToolStripMenuItem.PerformClick();
+        }
+
+        private void copyToolStripButton_Click(object sender, EventArgs e)
+        {
+            copyToolStripMenuItem.PerformClick();
+        }
+
+        private void pasteToolStripButton_Click(object sender, EventArgs e)
+        {
+            pasteToolStripMenuItem.PerformClick();
+        }
+    }
+}
